831bf742b381d48ec87246634fdd4d73df228166
Michal Gorczyca <mich.gorc@gmail.com>
2020-11-09 23:16:37 +0100

    Ścieżka patrolu

--- Assets/Scripts/Control/AIController.cs
+++ Assets/Scripts/Control/AIController.cs
@@ -4,20 +4,29 @@ using UnityEngine;
 using UnityEngine.AI;
 using RPGProject.Combat;
 using RPGProject.Core;
+using RPGProject.Movement;
 
 public class AIController : MonoBehaviour
 {
     [SerializeField] float chaseDistance = 5f;
+    [SerializeField] float suspicionTime = 5f;
 
     Fighter fighter;
     Health health;
     GameObject player;
+    Mover mover;
+
+    Vector3 guardPosition;
+    float timeSinceLastSawPlayer = Mathf.Infinity;
 
     private void Start() {
         
         fighter = GetComponent<Fighter>();
         health = GetComponent<Health>();
+        mover = GetComponent<Mover>();
         player = GameObject.FindWithTag("Player");
+
+        guardPosition = transform.position;
 
     }
 
@@ -25,15 +34,40 @@ public class AIController : MonoBehaviour
     {
         if(health.IsDead()) return;
         print("Enemy health is " + health.MyHealth());
-        if(AttackIfIsRange() && fighter.CanAttack(player)){
+        if(AttackIfIsRange() && fighter.CanAttack(player))
+        {
 
-            fighter.Attack(player);
+            timeSinceLastSawPlayer = 0f;
+            AttackBehaviour();
 
         }
-        else{
+        else if(timeSinceLastSawPlayer <= suspicionTime)
+        {
+            SuspiciousBehaviour();
 
-            fighter.Cancel();
         }
+        else
+        {
+            GuardBehaviour();
+        }
+
+        timeSinceLastSawPlayer += Time.deltaTime;
+    }
+
+    private void GuardBehaviour()
+    {
+        fighter.Cancel();
+        mover.StartMoveAction(guardPosition);
+    }
+
+    private void SuspiciousBehaviour()
+    {
+        GetComponent<ActionScheduler>().CancelCurrentAction();
+    }
+
+    private void AttackBehaviour()
+    {
+        fighter.Attack(player);
     }
 
     private bool AttackIfIsRange()
@@ -42,5 +76,12 @@ public class AIController : MonoBehaviour
         float distanceToPlayer = Vector3.Distance(transform.position, player.transform.position);
         return distanceToPlayer < chaseDistance;
         
+    }
+
+    //Called By Unity
+    private void OnDrawGizmosSelected() {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, chaseDistance);
+
     }
 }
---
+++ Assets/Scripts/Control/PatrolPath.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPGProject.Control{
+
+    public class PatrolPath : MonoBehaviour
+    {
+        const float waypointGizmosRadius = .3f;
+        private void OnDrawGizmos() {
+            
+            for(int i=0; i<transform.childCount;i++)
+            {
+                int j = GetNextIndex(i);
+                Gizmos.DrawSphere(GetPosition(i), waypointGizmosRadius);
+                Gizmos.DrawLine(GetPosition(i), GetPosition(j));
+                
+            }
+        }
+
+        private int GetNextIndex(int i){
+
+            if(i+1 == transform.childCount){
+
+                return 0;
+            }
+            else{
+
+                return i+1;
+            }
+
+        }
+
+        private Vector3 GetPosition(int i)
+        {
+            return transform.GetChild(i).position;
+        }
+    }
+
+}
